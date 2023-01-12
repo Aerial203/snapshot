84192ae3ee406cf6218953b26f66754b3ff66712
Ankit Shrestha <ankit.shrestha196@gmail.com>
2023-01-12 23:48:26 +0545

    remove func

--- src/components/Snapshot/Image.jsx
+++ src/components/Snapshot/Image.jsx
@@ -1,12 +1,8 @@
 import classes from "./Image.module.css"
 
 const Image = (props) => {
-    const handleClick = () => {
-        console.log("clicked")
-    }
-
     return <div className={classes["img-div"]}>
-        <img src={props.url} alt={props.alt} className={classes.img} onClick={handleClick}/>
+        <img src={props.url} alt={props.alt} className={classes.img} />
     </div>
 }
 
